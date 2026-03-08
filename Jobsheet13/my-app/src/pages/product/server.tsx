1804f562f35624e12cc3780e98bb1e4ce7711de9
Vincentius <leonandaprabowo@gmail.com>
2026-03-08 21:59:04 +0700

    cretae project

---
+++ Jobsheet13/my-app/src/pages/product/server.tsx
@@ -0,0 +1,24 @@
+import TampilanProduct from "../../views/product/index";
+import { ProductType } from "../../types/Product.type";
+
+const halamanProductServer = (props: { products: ProductType[] }) => {
+  const { products } = props;
+  return (
+    <div>
+      <h1>Halaman Produk Server</h1>
+      <TampilanProduct products={products} />
+    </div>
+  );
+};
+
+export default halamanProductServer;
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:3000/api/product");
+  const respone = await res.json();
+  return {
+    props: {
+      products: respone.data,
+    },
+  };
+}
