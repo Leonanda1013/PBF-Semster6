a6ef43c1b2d64c869bc103ba20ad7290163b54e4
Vincentius <leonandaprabowo@gmail.com>
2026-03-08 09:11:25 +0700

    3. Implementasi SSR

--- Jobsheet11/README.md
+++ Jobsheet11/README.md
@@ -11,12 +11,16 @@
 - **Kelas**: TI-3D
 
 ## Langkah 1 Membuat Dynamic Route
+
 ![alt text](images/image1.png)
 
 ## Langkah 2 Implementasi CSR (Client Rendering)
+
 ![alt text](images/image2-1.png)
 ![alt text](images/image2-2.png)
 ![alt text](images/image2-3.png)
 
-## Langkah 3
+## Langkah 3 Implementasi SSR
 
+![alt text](images/image3-1.png)
+![alt text](images/image3-2.png)
--- Jobsheet11/my-app/src/pages/product/[product].tsx
+++ Jobsheet11/my-app/src/pages/product/[product].tsx
@@ -1,17 +1,37 @@
 import fetcher from "@/utils/swr/fetcher";
 import { useRouter } from "next/router";
 import useSWR from "swr";
-
+import { ProductType } from "@/types/Product.type";
 import DetailProduct from "@/views/DetailProduct";
+// Client side
+// const HalamanProduct = () => {
+//   const { query } = useRouter();
+//   const { data, error, isLoading } = useSWR(`/api/product/${query.product}`, fetcher);
+//   return (
+//     <div>
+//       <DetailProduct products={isLoading ? [] : data.data} />
+//     </div>
+//   );
+// };
 
-const HalamanProduct = () => {
-  const { query } = useRouter();
-  const { data, error, isLoading } = useSWR(`/api/product/${query.product}`, fetcher);
+// server side
+const HalamanProduct = ({ product }: { product: ProductType }) => {
   return (
     <div>
-      <DetailProduct products={isLoading ? [] : data.data} />
+      <DetailProduct products={product} />
     </div>
   );
 };
 
 export default HalamanProduct;
+
+export async function getServerSideProps({ params }: { params: { product: string } }) {
+  const res = await fetch(`http://localhost:3000/api/product/${params?.product}`);
+  const respone = await res.json();
+
+  return {
+    props: {
+      product: respone.data,
+    },
+  };
+}
