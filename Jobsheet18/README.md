e54867c9ee8064bff925408b61c60b39170219bc
Vincentius <leonandaprabowo@gmail.com>
2026-04-07 23:47:41 +0700

    1. Optimasi gambar lokal

---
+++ Jobsheet18/README.md
@@ -0,0 +1,17 @@
+# Laporan Praktikum Jobsheet 17
+
+## Identitas
+
+- **Mata Kuliah**: Pemrograman Berbasis Framework
+- **Program Studi**: Teknik Informatika
+- **Semester**: 6
+- **Praktikum**: Jobsheet 18
+- **Nama**: Vincentius Leonanda Prabowo
+- **NIM**: 2341720149
+- **Kelas**: TI-3D
+
+
+## Langkah 1 - Optimasi Gambar Lokal
+![alt text](images/image1.png)
+
+## Langkah 2 -
--- Jobsheet18/my-app/src/pages/404.tsx
+++ Jobsheet18/my-app/src/pages/404.tsx
@@ -1,14 +1,14 @@
 import Link from "next/dist/client/link";
 import style from "../styles/404.module.scss";
-
+import Image from "next/image";
 const Custom404 = () => {
   return (
     <>
       <head>
         <title>404 - Page Not Found</title>
       </head>
-      <div className={style.eror}>
-        <img src="/page-not-found.png" alt="404" className={style.eror__image} />
+      <div className={style.error}>
+        <Image src="/page-not-found.png" alt="404" width={400} height={200} className={style.error__image} priority />
         <h1>404 - Page Not Found</h1>
         <p>Kamu Salah Masuk Halaman Wlekk.</p>
         <Link href="/">Kembali ke Beranda</Link>
